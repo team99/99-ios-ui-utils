8cbe45a4a550d46e67e0d7bc5055950f3026280b
Hai Pham <viethai.pham@holmusk.com>
2018-04-27 16:19:00 +0700

    chore(creation): created project files

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ui99' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ui99
+
+  target 'ui99Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ui99/ui99.h
@@ -0,0 +1,19 @@
+//
+//  ui99.h
+//  ui99
+//
+//  Created by Hai Pham on 27/4/18.
+//  Copyright © 2018 Swiften. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ui99.
+FOUNDATION_EXPORT double ui99VersionNumber;
+
+//! Project version string for ui99.
+FOUNDATION_EXPORT const unsigned char ui99VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ui99/PublicHeader.h>
+
+
